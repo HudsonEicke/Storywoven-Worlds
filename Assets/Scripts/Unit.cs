b2a90552eccc272f8cc50474938f19119d391437
Angelo White <angelowhite871@gmail.com>
2025-02-09 16:04:55 -0500

    Added Angelo Scene and some files in the assets tab
    Created some test characters to assign scripts to. Also, created folders to put prefabs, characters, background, and scripts.

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    // basic variables for a unit
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+    public int maxHP;
+    public int currentHP;
+
+}
