95b34ecafb749b5590487e6918e59744d9fe6c6a
Angelo White <angelowhite871@gmail.com>
2025-02-09 18:47:09 -0500

    Trying to fix the scale and working with 2D in a 3D space ;-;
    Sprites are acting weird, looking into it.

--- Assets/2D Scripts/BattleSystem.cs
+++ Assets/2D Scripts/BattleSystem.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+// We will use an ENUM to keep track of the battle state
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+    public BattleState state;
+
+
+    Unit playerUnit, enemyUnit;
+
+    // text variable
+    //public Text dialogueText;
     void Start()
     {
-        
+        state = BattleState.START; // We want to start the battle in the START state
+        BattleSetup();
     }
 
-    // Update is called once per frame
-    void Update()
+    // BattleSetup instantiates the player and enemy prefabs
+    void BattleSetup()
     {
-        
+        GameObject player = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = player.GetComponent<Unit>();
+        GameObject enemy = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemy.GetComponent<Unit>();
+
+        // Display unitname to player
+        //dialogueText.text = "NAME: " + enemyUnit.unitName + ".";
     }
 }
