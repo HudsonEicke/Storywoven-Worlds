141a28293614782ab977a56200a2ab39fab46e3c
Angelo White <angelowhite871@gmail.com>
2025-02-19 17:45:14 -0500

    Added a second player and more comments

--- Assets/2D Scripts/BattleSystem.cs
+++ Assets/2D Scripts/BattleSystem.cs
@@ -18,8 +18,8 @@ public class BattleSystem : MonoBehaviour
 
     Unit playerUnit, playerUnit2, enemyUnit;
 
-    public Text enemyHud, Player1Hud;
-    // Player2Hud, Player3Hud, Player4Hud;
+    public Text enemyHud, Player1Hud, Player2Hud;
+    // Player3Hud, Player4Hud;
 
     void Awake()
     {
@@ -63,6 +63,8 @@ public class BattleSystem : MonoBehaviour
     // BattleSetup will load in the prefabs and UIs for the battle and ends with updating it to either player turn or enemy turn
     void BattleSetup()
     {
+        // Need to find a way to have an int where I can show up characters that the player picks before the fight 
+        // and spawn them in the order they want, possibly a list or hashmap.
         GameObject player = Instantiate(playerPrefab, playerBattleStation);
         GameObject player2 = Instantiate(playerPrefab2, playerBattleStation2);
         playerUnit = player.GetComponent<Unit>();
@@ -73,11 +75,18 @@ public class BattleSystem : MonoBehaviour
         // Display unitname to player
         enemyHud.text = enemyUnit.unitName;
         Player1Hud.text = playerUnit.unitName;
-        // Player2Hud.text = playerUnit.unitName;
+        Player2Hud.text = playerUnit2.unitName;
         // Player3Hud.text = playerUnit.unitName;
         // Player4Hud.text = playerUnit.unitName;
 
+        // Probably do health bar here
+
+
         // starting player's turn
-        GameManager2D.instance.UpdateBattleState(BattleState.PLAYERTURN);
+        // we can probably have a boolean to decide who starts first
+        if (true) // I'll change this later
+            GameManager2D.instance.UpdateBattleState(BattleState.PLAYERTURN);
+        else
+            GameManager2D.instance.UpdateBattleState(BattleState.ENEMYTURN);
     }
 }
