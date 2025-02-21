eb04521895dfc30bc34ce9c58858dabb607dce1d
HudsonEicke <123416403+HudsonEicke@users.noreply.github.com>
2025-02-20 19:17:25 -0500

    Started work on item button class

---
+++ Assets/ItemButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ItemButton : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text itemName;
+    [SerializeField]
+    private TMP_Text quantity;
+    [SerializeField]
+    private Image itemImage;
+    [SerializeField]
+    private GameObject buttonObject;
+
+
+    public void UpdateButton(Item newItem)
+    {
+        itemName.text = newItem.name;
+        quantity.text = newItem.quantity.ToString();
+        itemImage.sprite = newItem.itemIcon;
+    }
+
+    public void Use()
+    {
+        //need a call to the UI manager
+    }
+
+    public void ActivateButton()
+    {
+        buttonObject.SetActive(true);
+    }
+
+    public void DeactivateButton()
+    {
+        buttonObject.SetActive(false);
+    }
+}
