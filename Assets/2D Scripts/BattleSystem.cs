794fbfb7a7b813a4121b156cc45d8753fb000fe4
Angelo White <angelowhite871@gmail.com>
2025-02-23 17:31:44 -0500

    Finished basic menu
    Basic menu where the player can now select attack at a certain character and do damage is done.

--- Assets/2D Scripts/BattleSystem.cs
+++ Assets/2D Scripts/BattleSystem.cs
@@ -13,11 +13,15 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] public List<Transform> playerBattleStations;
     [SerializeField] public List<GameObject> playerPrefab;
     public List<GameObject> player;
+    public List<GameObject> buttons;
+    public List<GameObject> buttonsForPlayer;
     public Transform enemyBattleStation;
     public BattleState state;
 
+
     // variables for buttons
-    public GameObject buttonPrefabAlly1, buttonPrefabAlly2, buttonPrefabAlly3, buttonPrefabAlly4;
+    [SerializeField] public List<GameObject> buttonPrefabAllies;
+    [SerializeField] public List<GameObject> buttonPrefabsAttack;
     public Transform buttonPanel;
 
 
@@ -25,6 +29,7 @@ public class BattleSystem : MonoBehaviour
 
     public Text enemyHud;
     [SerializeField] public List<Text> playerHuds;
+    [SerializeField] public List<Text> playerHudsAttack;
     public List<Unit> playerUnits;
 
     void Awake()
@@ -32,6 +37,10 @@ public class BattleSystem : MonoBehaviour
         // We want to do this during awake since Start() does it in a different order sometimes
         Debug.Log("[BattleSystem]Subscribing to event");
         GameManager2D.OnBattleStateChanged += OnBattleStateChanged; 
+        foreach (var hud in playerHudsAttack)
+        {
+            hud.gameObject.SetActive(false);
+        }
     }
 
     // Called when battle state changes
@@ -53,6 +62,7 @@ public class BattleSystem : MonoBehaviour
                 break;
             case BattleState.ENEMYTURN:
                 // Handle enemy's turn logic here
+                GameManager2D.instance.UpdateBattleState(BattleState.PLAYERTURN);
                 break;
             case BattleState.WON:
                 // Handle winning logic
@@ -70,28 +80,12 @@ public class BattleSystem : MonoBehaviour
     // BattleSetup will load in the prefabs and UIs for the battle and ends with updating it to either player turn or enemy turn
     void BattleSetup()
     {
-        // instantiate players
-        for (int i = 0; i < 2; i++) {
-            player.Add(Instantiate(playerPrefab[i], playerBattleStations[i]));
-            playerUnits.Add(player[i].GetComponent<Unit>());
-            Character firstCharacter = GameManager2D.instance.characterList.characters[i];
-            playerUnits[i].SetStats(firstCharacter.health, firstCharacter.attack, firstCharacter.defense, 100, firstCharacter.name, 1, firstCharacter.energy);
-        }
+        instantiatePlayers();
 
-        // instantiate enemies
+        // instantiate enemies (I'll change this to a list later)
         GameObject enemy = Instantiate(enemyPrefab, enemyBattleStation);
         enemyUnit = enemy.GetComponent<Unit>();
         enemyUnit.SetStats(100, 10, 5, 100, "Enemy", 1, 100);
-
-        // Display unitname to player
-        for (int i = 0; i < 4; i++) {
-           if (i < playerUnits.Count) {
-               playerHuds[i].text = playerUnits[i].unitName;
-           }
-           else {
-               playerHuds[i].text = "Empty";
-           }
-        }
         enemyHud.text = enemyUnit.unitName;
 
         // Probably do health bar here
@@ -106,11 +100,64 @@ public class BattleSystem : MonoBehaviour
 
     void playerTurnSetup() {
         // spawn the buttons to select which character to use
-        GameObject button1 = Instantiate(buttonPrefabAlly1, buttonPanel);
-        GameObject button2 = Instantiate(buttonPrefabAlly2, buttonPanel);
-        GameObject button3 = Instantiate(buttonPrefabAlly3, buttonPanel);
-        GameObject button4 = Instantiate(buttonPrefabAlly4, buttonPanel);
-        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(button1);
+        for (int i = 0; i < playerUnits.Count; i++) {
+            int index = i; // create local copy of i for the lambda function
+            buttons.Add(Instantiate(buttonPrefabAllies[i], buttonPanel));
+            buttons[i].GetComponent<Button>().onClick.AddListener(() => OnButtonClicked(index));
+        }
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(buttons[0]);
+    }
 
+    void instantiatePlayers() {
+        // instantiate players
+        // would like to make it to where the 3D game manager will give some variables for current
+        // HP and other current stats for the player. The file will hold the overal stats
+        for (int i = 0; i < playerPrefab.Count; i++) {
+            player.Add(Instantiate(playerPrefab[i], playerBattleStations[i]));
+            playerUnits.Add(player[i].GetComponent<Unit>());
+            Character firstCharacter = GameManager2D.instance.characterList.characters[i];
+            playerUnits[i].SetStats(firstCharacter.health, firstCharacter.attack, firstCharacter.defense, 0, firstCharacter.name, 0, firstCharacter.energy);
+        }
+
+        // Display unitname to player
+        for (int i = 0; i < 4; i++) {
+           if (i < playerUnits.Count) {
+               playerHuds[i].text = playerUnits[i].unitName;
+           }
+           else {
+               playerHuds[i].text = "Empty";
+           }
+        }
+    }
+
+    void ToggleTextFirst(int index) {
+        playerHuds[index].gameObject.SetActive(!playerHuds[index].gameObject.activeSelf);
+    }
+
+    void ToggleTextSecond(int index) {
+        // Deal with the text here
+        playerHudsAttack[index].text = "Attack";
+        playerHudsAttack[index].gameObject.SetActive(!playerHudsAttack[index].gameObject.activeSelf);
+
+        // get rid of the buttons and then spawn in the next set of button options for that character
+        for (int i = 0; i < playerUnits.Count; i++) {
+            buttons[i].SetActive(false);
+        }
+        buttonsForPlayer.Add(Instantiate(buttonPrefabsAttack[index], buttonPanel));
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(buttonsForPlayer[0]);
+
+        // buttons clicked
+        buttonsForPlayer[0].GetComponent<Button>().onClick.AddListener(() => AttackButtonClicked(index));
+    }
+
+    void OnButtonClicked(int index) {
+        Debug.Log("Button " + index );
+        ToggleTextFirst(index); // get rid of the text
+        ToggleTextSecond(index); // spawn in next buttons and text for decisions
+    }
+
+    void AttackButtonClicked(int index) {
+        enemyUnit.healthChange(-playerUnits[index].unitAttack());
+        GameManager2D.instance.UpdateBattleState(BattleState.ENEMYTURN);
     }
 }
