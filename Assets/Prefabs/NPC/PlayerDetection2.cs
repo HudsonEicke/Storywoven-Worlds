3d95a3307c413b81b19a0f915eb38d1b17010b9c
Prebosss <kennethar2004@gmail.com>
2025-04-20 17:16:05 -0400

    3 NPCs and Instructions

---
+++ Assets/Prefabs/NPC/PlayerDetection2.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerDetection2 : MonoBehaviour
+{
+    public GameObject interactButton;
+    public GameObject dialogueStuff;
+    private bool playerDetected = false;
+    private DTNoButt dialogueTrigger;
+
+    public void Start()
+    {  
+        dialogueTrigger = GetComponent<DTNoButt>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            playerDetected = true;
+            if (interactButton != null)
+            {
+                interactButton.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("Interact button not assigned");
+            }
+            Debug.Log("Player detected!");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            playerDetected = false;
+            if (interactButton != null)
+            {
+                interactButton.SetActive(false);
+            }
+            if (dialogueStuff != null)
+            {
+                dialogueStuff.SetActive(false);
+            }
+            else
+            {
+                Debug.LogWarning("Interact button not assigned");
+            }
+            if (dialogueTrigger != null)
+            {
+                dialogueTrigger.EndDialogue();
+            }
+            else
+            {
+                Debug.LogWarning("DialogueTrigger not assigned");
+            }
+            Debug.Log("Player left the area!");
+        }
+    }
+
+    private void Update()
+    {
+        if (playerDetected && Input.GetKeyDown(KeyCode.F))
+        {
+            if (interactButton != null)
+            {
+                interactButton.SetActive(false);
+
+            }
+            else
+            {
+                Debug.LogWarning("Interact button not assigned");
+            }
+
+            if (dialogueStuff != null)
+                {
+                    dialogueStuff.SetActive(true);
+                }
+            else
+            {
+                Debug.LogWarning("Dialogue stuff not assigned");
+            }
+
+            if(dialogueTrigger != null)
+            {
+                dialogueTrigger.TriggerDialogue();
+            }
+            else
+            {
+                Debug.LogWarning("DialogueTrigger not assigned");
+            }
+
+            // Call the method to trigger the dialogue or any other action
+            Debug.Log("Inteacted with F");
+
+        }
+    }
+}
